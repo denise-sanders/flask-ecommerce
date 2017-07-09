d9fa20aa47a9edf48eeaf037f9b80acf9785a24d
deniseirvin <dairvin9@tamu.edu>
2017-07-09 09:56:19 -0400

    added ssl capabilities

--- app/models.py
+++ app/models.py
@@ -1,10 +1,20 @@
+from app import app
 from app import db
+from flask_login import LoginManager, UserMixin
 import enum
 
 class DressStatus(enum.Enum):
-    one = 1
-    two = 2
-    three = 3
+    purchasable = 1
+    sold = 2
+    delivered = 3
+    removed = 4
+    # more error code might be relevant
+
+
+lm = LoginManager(app)
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
 class User(db.Model):
@@ -14,6 +24,8 @@ class User(db.Model):
     name_full = db.Column(db.String(64), index=True)
     email = db.Column(db.String(120), index=True, unique=True)
     selling = db.relationship('Dress', backref='seller', lazy='dynamic')
+    bought = db.relationship('Dress', backref='buyer', lazy='dynamic')
+
 
     def __repr__(self):
         return '<User %r>' % (self.name_full)
@@ -24,6 +36,8 @@ class Dress(db.Model):
     timestamp = db.Column(db.DateTime)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     status = db.Column(db.Enum(DressStatus))
+    social_id = db.Column(db.String(64), nullable=False, unique=True) # which service they use to log in
+    email = db.Column(db.String(64), nullable=True)
     # can I make the status an enum type value, as sold, on the market, removed, closed, in process
     # add multiple picutres to be allowed (perhaps up to three?)
     def __repr__(self):
--- run.py
+++ run.py
@@ -1,3 +1,30 @@
 #!flask/bin/python
 from app import app
-app.run(debug=True)
+from pathlib import Path
+import os.path
+
+import sys
+
+port = 5000
+
+try:
+    cert_file = app.config['CERT_FILE']
+    key_file = app.config['KEY_FILE']
+
+    if not os.path.isfile(cert_file):
+        raise FileNotFoundError
+    if not os.path.isfile(key_file):
+        raise FileNotFoundError
+
+    context = (cert_file, key_file)
+    app.run(host='0.0.0.0', port=port, ssl_context=context, threaded=True, debug=True)
+
+except FileNotFoundError as e:
+    try:
+        print("SSL Failed, could not find cert of key file.\nStarting server without ssl") #TODO: remove this in prod
+        app.run(host='0.0.0.0', port=port, threaded=True, debug=True)
+    except:
+        print("Starting server without ssl failed")
+        exit(1)
+except Exception as e:
+    print("Unknown error {0}".format(type(e).__name__))
