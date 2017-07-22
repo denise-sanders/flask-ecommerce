5b00a8d9f65fc49fa509a9f1218f627075c10cf2
deniseirvin <dairvin9@tamu.edu>
2017-07-21 20:10:45 -0400

    added templates, login.html is busted

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
-app = Flask(__name__)
+app = Flask(__name__,static_url_path="", static_folder="static") # , static_url_path='/static'
+#app.static_folder = 'static'
 app.config.from_object('config')
 db = SQLAlchemy(app)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True #can probably set to false
+
+lm = LoginManager()
+lm.init_app(app)
 
 from app import views, models
